b37b855a149aebd000fd6add8157e7f395bcc185
James Yoo <jimboyoo@gmail.com>
2019-10-17 22:52:20 -0700

    Add slider, colorHelpers to generate color levels

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
+    
     <div>
-      <Palette {...seedColors[1]}/>
+      <Palette palette={generatePalette(seedColors[4])}/>
     </div>
   );
 }
