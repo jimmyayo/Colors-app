214cde178cd1da3482df855cd38d8549575f0d0d
James Yoo <jimboyoo@gmail.com>
2019-11-05 08:43:29 -0800

    singlePalette responsive, add responsive helper

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,5 +1,5 @@
 import chroma from 'chroma-js';
-
+import sizes from './sizes';
 
 export default {
     ColorBox: {
@@ -12,7 +12,19 @@ export default {
         marginBottom: "-3.8px",
         "&:hover button": {
             opacity: 1
-        }
+        },
+        [sizes.down("lg")]: {
+            width: "25%",
+            height: props => props.showingFullPalette ?  "20%" : "10%"
+        },
+        [sizes.down("md")]: {
+            width: "50%",
+            height: props => props.showingFullPalette ?  "10%" : "10%"
+        },
+        [sizes.down("xs")]: {
+            width: "100%",
+            height: props => props.showingFullPalette ?  "5%" : "10%"
+        },
     },
     copyText: {
         color: props => chroma(props.background).luminance() >= 0.65 ? "black" : "white"
